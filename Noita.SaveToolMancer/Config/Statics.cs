de2578d985dc24c1d4a07836944841d74ed46453
PepeMage <you@example.com>
2025-08-31 13:50:26 +0200

    Added check to prevent save and restore during running process

--- Noita.SaveToolMancer/Config/Statics.cs
+++ Noita.SaveToolMancer/Config/Statics.cs
@@ -4,5 +4,7 @@
     {
         internal static string GameFolderName = "Nolla_Games_Noita";
         internal static string DefaultSaveName = "save00";
+        internal static string GameProcess = "noita";
+        internal static string GameProcess2 = "noita_dev";
     }
 }
--- Noita.SaveToolMancer/Controllers/SaveController.cs
+++ Noita.SaveToolMancer/Controllers/SaveController.cs
@@ -1,4 +1,5 @@
-﻿using Noita.SaveToolMancer.Helpers;
+﻿using Noita.SaveToolMancer.Config;
+using Noita.SaveToolMancer.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,11 +11,20 @@ namespace Noita.SaveToolMancer.Controllers
     {
         internal static KeyValuePair<bool, string> BackupSaveGame()
         {
+            if (IsNoitaRunning())
+            {
+                return new KeyValuePair<bool, string>(false,"Quit Noita to save the game first!");
+            }
             return SaveHelper.CopySaveGame();
         }
 
         internal static KeyValuePair<bool, string> RestoreSaveGame()
         {
+            if (IsNoitaRunning())
+            {
+                return new KeyValuePair<bool, string>(false, "Quit Noita to restore a game save!");
+            }
+
             return SaveHelper.RestoreSaveGame();
         }
 
@@ -36,5 +46,25 @@ namespace Noita.SaveToolMancer.Controllers
             return new KeyValuePair<bool, string> (true, string.Empty);
         }
 
+        internal static bool IsNoitaRunning()
+        {
+            bool isRunning = IsProcessRunning(Statics.GameProcess);
+            bool isDevRunning = IsProcessRunning(Statics.DevGameProcess);
+
+            if (isRunning || isDevRunning)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static bool IsProcessRunning(string processName)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            return processes.Length > 0;
+        }
     }
+
+    
 }
