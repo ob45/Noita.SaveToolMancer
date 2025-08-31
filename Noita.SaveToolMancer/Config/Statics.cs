6aa3362d8b47699004c7fbdfe5626c7e453ae344
PepeMage <you@example.com>
2025-08-31 13:51:22 +0200

    Changed process name var for dev process

--- Noita.SaveToolMancer/Config/Statics.cs
+++ Noita.SaveToolMancer/Config/Statics.cs
@@ -5,6 +5,6 @@
         internal static string GameFolderName = "Nolla_Games_Noita";
         internal static string DefaultSaveName = "save00";
         internal static string GameProcess = "noita";
-        internal static string GameProcess2 = "noita_dev";
+        internal static string DevGameProcess = "noita_dev";
     }
 }
